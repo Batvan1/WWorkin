64c34b66cc107b213ef4b8d26aad21212feb09b2
Batuhan Öztüfekçi <krayyark8@gmailcom>
2025-12-26 16:10:57 +0300

    fiveCommit mini fix

--- app/api/store/dashboard/route.js
+++ app/api/store/dashboard/route.js
@@ -1,37 +1,37 @@
 import prisma from "@/lib/prisma"
 import authSeller from "@/middlewares/authSeller"
-import { getAuth } from "@clerk/nextjs/dist/types/server" 
+import { getAuth } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 
 
 // get dashboard data for seller (total orders, total earnings, total products)
-export async function GET(request){
+export async function GET(request) {
     try {
-        const {userId} = getAuth(request)
+        const { userId } = getAuth(request)
         const storeId = await authSeller(userId)
 
         // get all orders for seller
-        const orders = await prisma.order.findMany({where: {storeId}})
+        const orders = await prisma.order.findMany({ where: { storeId } })
 
         // get all prodcuts with raitings for seller
-        const products = await prisma.product.findMany({where: {storeId}})
+        const products = await prisma.product.findMany({ where: { storeId } })
 
         const ratings = await prisma.rating.findMany({
-            where: {productId: {in: products.map(product => product.id)}},
-            include: {user: true, product: true}
+            where: { productId: { in: products.map(product => product.id) } },
+            include: { user: true, product: true }
         })
 
         const dashboardData = {
             ratings,
             totalOrders: orders.length,
-            totalEarnings: Math.round(orders.reduce((acc, order)=> acc + order.total, 0 )),
+            totalEarnings: Math.round(orders.reduce((acc, order) => acc + order.total, 0)),
             totalProducts: products.length
         }
 
-        return NextResponse.json({dashboardData})
+        return NextResponse.json({ dashboardData })
 
     } catch (error) {
         console.error(error)
-        return NextResponse.json({error: error.code || error.message}, {status: 400})
+        return NextResponse.json({ error: error.code || error.message }, { status: 400 })
     }
 }
